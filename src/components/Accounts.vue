f4f539e2ce698d9a7dc97bb1a8d57b3e2d2df435
serkanaydin <serkanaydin.cse@gmail.com>
2025-02-27 16:13:40 +0300

    add persistent store input restrictions remove added

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -14,18 +14,20 @@
         <div class="col-3 text-start fw-bold">Пароль</div>
       </div>
       <div class="row mt-1" v-for="(account,index) in accountStore.accounts" :key="index">
-        <div class="col-3">
-          <InputText v-model="account.label" maxlength="50" autofocus fluid style="height: 50px"/>
+        <div class="col-2" >
+          <InputText :ref="(el:any) => registerRef(index, 'label',el)" :value="account.label" @input="onChangeInput(account,index,'label',$event)"  autofocus fluid style="height: 50px"/>
         </div>
         <div class="col-3">
-          <Select v-model="account.accountType" placeholder="Select Account Type" style="height: 50px" :options="accountTypes" autofocus fluid />
+          <Select :ref="(el:any) => registerRef(index, 'accountType',el)"  :value="account.accountType" @input="onChangeInput(account,index,'accountType',$event)"  placeholder="Select Account Type" style="height: 50px" :options="accountTypes" autofocus fluid />
         </div>
         <div :class="account.accountType === 'LDAP'? 'col-6' : 'col-3'">
-          <InputText v-model="account.login"  maxlength="100" autofocus fluid style="height: 50px" />
+          <InputText :ref="(el:any) => registerRef(index, 'login',el)" :value="account.login"  @input="onChangeInput(account,index,'login',$event)"  autofocus fluid style="height: 50px"/>
         </div>
-
         <div class="col-3" v-if="account.accountType !== 'LDAP'">
-            <Password v-model="value" :required="account.accountType==='Локальная'" toggleMask autofocus fluid style="height:50px"/>
+            <Password :ref="(el:any) => registerRef(index, 'password',el)"  :value="account.password" @input="onChangeInput(account,index,'password',$event)"  :required="account.accountType==='Локальная'" toggleMask autofocus fluid style="height:50px"/>
+        </div>
+        <div class="col-1">
+          <Button icon="bi bi-trash" @click="onClickRemove(index)"/>
         </div>
       </div>
     </div>
@@ -35,10 +37,12 @@
 <script setup lang="ts">
 
   import { useAccountsStore, IAccount } from '@/store/accounts';
+  import {ref} from "vue";
 
   const accountStore = useAccountsStore();
   const {add,remove} = accountStore;
   let accountTypes = ['Локальная','LDAP']
+  const inputRefs = ref({});
 
   function onClickAdd (){
     let newAccount: IAccount = {label:"",accountType:"",login:"",password:""} as IAccount;
@@ -48,6 +52,35 @@
   function onClickRemove(index: number){
     let account = accountStore.accounts.at(index) as IAccount;
     accountStore.remove(account);
+  }
+
+  const registerRef = (index:number,field:string, el:any) => {
+    const inputRef = `${index}${field}`;
+    inputRefs.value[inputRef] = el;
+  };
+
+  function completeValidation(account: IAccount,el:any, field:string,validationResult :boolean, value: string ){
+    if(validationResult){
+      account[`${field}`]= value;
+      el.$el.classList.remove('border-danger');
+    }
+    else{
+      el.$el.classList.add('border-danger');
+    }
+  }
+
+  function onChangeInput(account : IAccount ,index:number,field:string,event:any){
+    event.stopPropagation();
+        const inputRef = `${index}${field}`;
+        const el = inputRefs.value[inputRef];
+        let validationResult:boolean=false;
+        switch (field){
+          case 'label': validationResult = event.target.value.length <= 50; break;
+          case 'login': validationResult = event.target.value.length <= 100; break;
+          case 'password': validationResult = event.target.value.length <= 100; break;
+
+        }
+        completeValidation(account,el,field,validationResult,event.target.value);
   }
 
 </script>
